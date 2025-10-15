c2e559a4db5a049deb5f4646e30c4fef6449bd6d
Jun Song <65651754+early-riser18@users.noreply.github.com>
2025-10-15 14:22:36 +0200

    feat: new use case section

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,16 @@ function scrollToAnchor(id, offset = 0) {
     window.scrollTo({ top: y, behavior: 'smooth' })
   }
   
+async function getUseCases() {
+    const response = await fetch('/src/use-cases.json', { cache: 'no-cache' });
+    if (!response.ok) return {};
+    return await response.json();
+}
+
+async function getUseCaseCategories() {
+    const useCasesRecord = await getUseCases();
+    const categories = Object.values(useCasesRecord)
+        .map(item => item && item.category)
+        .filter(Boolean);
+    return Array.from(new Set(categories));
+}
